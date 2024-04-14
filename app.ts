21c06ea6e4bb3c839df3c0721aae28733344d7c0
Madeel1234 <164627451+Madeel1234@users.noreply.github.com>
2024-04-14 15:20:32 +0500

    Add files via upload

---
+++ app.ts
@@ -0,0 +1,11 @@
+#! /usr/bin/env node
+
+// Solution # 20
+// Think of something you could store in a array. for example, you could make a list of mountains, rivers, countries, cities,
+//languages or anything else you'd like. write a program that create a list containing these items. 
+
+let languages:string[] = ['English','Urdu','Hindhi','Arabic','French']
+console.log("list of languages")
+for(let top of languages){
+    console.log(top)
+}
